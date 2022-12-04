975f152c71434b575765d253856b9f3ced6008a2
Helmuth Risch <helmuthfilho001@gmail.com>
2022-12-03 23:31:11 -0300

    WIP First commit / project initialization

---
+++ assets/index.renderer.js
@@ -0,0 +1,9 @@
+const informationElement = document.getElementById('info')
+informationElement.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const func = async () => {
+  const response = await window.versions.ping()
+  console.log(response) // prints out 'pong'
+}
+
+func()
